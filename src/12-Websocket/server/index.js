5327dee03a2d984f83e7f2ecebc291ab36edc519
WJX2001 <1758807220@qq.com>
2023-08-03 11:00:07 +0800

    feat: 聊天室的退出提示

--- src/12-Websocket/server/index.js
+++ src/12-Websocket/server/index.js
@@ -5,6 +5,11 @@ const wss = new WebSocket.Server({port:8000})
 wss.on('connection',function(ws) {
   console.log('a new client is connected')
   ws.on('message',function(msg) {
+    // 检查消息是否是一个退出消息
+    if (msg.includes('退出了聊天室')) {
+      console.log(msg); // 打印退出消息
+    }
+    
     // 广播给非自己的其他客户端
     wss.clients.forEach(function each(client) {
       if(client !== ws && client.readyState === WebSocket.OPEN) {
